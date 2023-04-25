da96b897d54ff8f8882a70408c875b494d067c13
Gualberto2 <elijahwilsonnn@gmail.com>
2023-04-24 17:18:55 -0700

    comitmmamms

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import {useLocation, useNavigate} from "react-router-dom"
 export default function Header() {
   const location = useLocation();
   const navigate = useNavigate();
-  function pathMathRoute(route){
+  function pathMatchRoute(route){
     if (route === location.pathname){
-      return true
+      return true;
     }
 }
   return (
@@ -17,9 +17,9 @@ export default function Header() {
         </div>
         <div>
           <ul className="flex space-x-10">
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500"}`} onClick={()=>navigate("/offers")}>Offers</li>
-            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`} onClick={()=>navigate("/sign-in")}>Sign In</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/") && "text-black border-b-red-500"}`} onClick={()=>navigate("/")}>Home</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/offers") && "text-black border-b-red-500"}`} onClick={()=>navigate("/offers")}>Offers</li>
+            <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute("/sign-in") && "text-black border-b-red-500"}`} onClick={()=>navigate("/sign-in")}>Sign In</li>
           </ul>
         </div>
       </header>
--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
-import {FcGoogle} from "react-icons/fc"
-import { GoogleAuthProvider } from "firebase/auth";
+import {FcGoogle} from "react-icons/fc";
+import { GoogleAuthProvider, getAuth, signInWithPopup} from "firebase/auth";
+import { toast } from 'react-toastify'
+import {db} from "../firebase"
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 
-function onGoogleClick(){}
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick(){
+    try {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user  = result.user
+  
+      //check for users
+    
+      const docRef = doc(db,"users", user.uid)
+      const docSnap = await getDoc(docRef)
+  
+      if(!docSnap.exists()){
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        });
+      }
+      navigate("/")
+    } catch (error) {
+      toast.error("Could not authorize with Google")
+    }
+  }
   return (
     <button type="button" onClick={onGoogleClick} className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
         <FcGoogle
@@ -12,3 +40,5 @@ export default function OAuth() {
     </button>
   )
 }
+
+
