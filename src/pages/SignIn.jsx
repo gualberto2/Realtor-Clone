ab99389232c66c7f8c6f0dc8008ea0896ef5ab31
Gualberto2 <elijahwilsonnn@gmail.com>
2023-04-24 18:47:00 -0700

    sign in with firebase working

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {useState} from "react";
 import {AiFillEyeInvisible, AiFillEye} from "react-icons/ai";
-import {Link} from "react-router-dom";
-import OAuth from "../components/OAuth"
+import {Link, useNavigate} from "react-router-dom";
+import OAuth from "../components/OAuth";
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { toast } from "react-toastify"
  
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false)
@@ -11,11 +13,24 @@ export default function SignIn() {
     password: "",
   });
   const {email, password} = formData;
+  const navigate = useNavigate();
   function onChange(e){
     setFormData((prevState)=>({
       ...prevState,
       [e.target.id]: e.target.value,
    }))
+  }
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential =  await signInWithEmailAndPassword (auth, email, password)
+      if (userCredential.user){
+        navigate("/")
+      }
+    } catch (error) {
+      toast.error("Bad user credentials")
+    }
   }
   return (
     <section>
@@ -28,7 +43,7 @@ export default function SignIn() {
           className="w-full rounded-2xl" />
         </div>
         <div  className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
              type="email"
              id="email" 
