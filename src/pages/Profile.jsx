998af95551efb976420e0e41c676d69226e95a92
Gualberto2 <elijahwilsonnn@gmail.com>
2023-04-25 06:17:47 -0700

    starting profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <>
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+      <div>
+        <form action="">
+          
+        </form>
+      </div>
+    </section>
+    </>
   )
 }
