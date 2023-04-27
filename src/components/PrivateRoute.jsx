2b65a1bfcc1088a3b329c834e050f8bd9703993a
Gualberto2 <elijahwilsonnn@gmail.com>
2023-04-26 18:22:23 -0700

    finished edit profile and apply changes to fb

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,5 +3,8 @@ import {Outlet, Navigate} from "react-router-dom"
 import {useAuthStatus} from '../hooks/useAuthStatus';
 export function PrivateRoute() {
     const {loggedIn, checkingStatus} = useAuthStatus();
+    if (checkingStatus) {
+      return <h3>Loading...</h3>
+    }
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in"/>;
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,15 @@
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
 import React from 'react'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
 
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetail,  setChangeDetail] = useState(false);
   const [formData, setFormData] =  useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
@@ -16,6 +20,33 @@ export default function Profile() {
     auth.signOut()
     navigate("/")
   }
+
+  function onChange(e){
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+  async function onSubmit(){
+ try {
+  if (auth.currentUser.displayName !== name){
+     //update display name in firebase auth
+     await updateProfile(auth.currentUser,{
+      displayName: name,
+     })
+
+     // update name in the firestore
+
+     const docRef = doc(db, "users", auth.currentUser.uid)
+     await updateDoc(docRef, {
+      name, 
+     })
+     toast.success('Profile details updated successfully!')
+  }
+ } catch (error) {
+  toast.error("Could not update the profile details")
+ }
+  }
   return (
     <>
     <section className="max-w-6xl mx-auto flex justify-center items-center flex-col ">
@@ -24,15 +55,23 @@ export default function Profile() {
         <form action="">
           {/* {Name Input} */}
 
-          <input type="text" id="name" value={name} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out " />
+          <input type="text" id="name" value={name} disabled={!changeDetail} onChange={onChange} className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`} />
 
           {/* {Email Input} */}
 
-          <input type="email" id="email" value={email} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out " />
+          <input type="email" id="email" value={email} disabled={!changeDetail} onChange={onChange}className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out " />
 
           <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
             <p className='flex items-center mb-6 '>Do you want to change your name?
-            <span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>Edit</span>
+            <span 
+            className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer' 
+            onClick={()=>{
+              changeDetail && onSubmit();
+              setChangeDetail((prevState) => !prevState)
+            }}
+            >
+              {changeDetail ? "Apply Change" : "Edit"}
+            </span>
             </p>
             <p onClick={onLogout} className='text-blue-600 hover:text-blue-800 cursor-pointer transition duration-200 ease-in-out'>Sign out</p>
           </div>
