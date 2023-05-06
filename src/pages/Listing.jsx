02c27473316078806d2c62fa3d5bb49f88977f3f
Gualberto2 <elijahwilsonnn@gmail.com>
2023-05-06 08:10:28 -0700

    finished listing page with contact landlord component,

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -19,12 +19,16 @@ import {
   FaParking,
   FaChair,
 } from "react-icons/fa";
+import { getAuth } from "firebase/auth";
+import Contact from "../components/Contact";
 
 export default function Listing() {
+  const auth = getAuth();
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
   const [shareLinkCopied, setShareLinkCopied] = useState(false);
+  const [contactLandlord, setContactLandlord] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
   useEffect(() => {
     async function fetchListing() {
@@ -81,7 +85,7 @@ export default function Listing() {
       )}
 
       <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg border-3 shadow-lg bg-white lg:space-x-5">
-        <div className=" w-full h-[200px] lg-[400px] ">
+        <div className=" w-full">
           <p className="text-2xl font-bold mb-3 text-blue-900">
             {listing.name} - ${" "}
             {listing.offer
@@ -115,7 +119,7 @@ export default function Listing() {
               Description - {listing.description}
             </span>
           </p>
-          <ul className="flex  items-center space-x-2 sm:space-x-10 text-sm font-semibold">
+          <ul className="flex  items-center space-x-2 sm:space-x-10 text-sm font-semibol mb-6">
             <li className="flex items-center whitespace-nowrap">
               <FaBed className="text-lg mr-1" />
               {+listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
@@ -137,6 +141,19 @@ export default function Listing() {
                 : "Not Furnished"}
             </li>
           </ul>
+          {listing.userRef != auth.currentUser?.uid && !contactLandlord && (
+            <div className="mt-6">
+              <button
+                onClick={() => setContactLandlord(true)}
+                className="px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg w-full text-center transition ease-in-out duration-150  z-9"
+              >
+                Contact Landlord
+              </button>
+            </div>
+          )}
+          {contactLandlord && (
+            <Contact userRef={listing.userRef} listing={listing} />
+          )}
         </div>
         <div className="bg-blue-300 w-full h-[200px] lg-[400px] z-10 overflow-x-hidden "></div>
       </div>
